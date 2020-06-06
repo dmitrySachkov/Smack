74dfaa468ee1e8f653ce26b82d8f32e07e422ee6
Dmitry <d.a.sachkov@gmail.com>
2020-06-07 00:13:15 +0300

    Add the LoginVC, add the LoginVC interface

---
+++ Smack/New Group/LoginVC.swift
@@ -0,0 +1,27 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Дмитрий on 07.06.20.
+//  Copyright © 2020 Sachkov Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    //Outlets
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+    @IBAction func clousePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Дмитрий on 06.06.20.
+//  Copyright © 2020 Sachkov Dmitry. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
